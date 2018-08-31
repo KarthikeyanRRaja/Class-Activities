1cbd87e0e595db74384784cca9137e2a1283351d
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-08-31 19:56:48 +0530

    Add files via upload

---
+++ num_reverse.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class num_reverse {
+	public static void main(String ar[])
+	{
+		Scanner s=new Scanner(System.in);
+	String str;
+	System.out.println("Enter the Integer value=");
+	str=s.nextLine();
+	StringBuilder str1=new StringBuilder(str); 
+}
+}
